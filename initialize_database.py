a000f9a957078db1baf402bdfaf923a1bfe83a61
Simon Forman <forman.simon@gmail.com>
2013-04-11 16:36:28 -0700

    Split out DB from Login stuff.

--- initialize_database.py
+++ initialize_database.py
@@ -1,7 +1,11 @@
 import sys
-from login_stuff import db, User
+from login_stuff import app, db, User
+
+app.test_request_context().push()
+
 if len(sys.argv) > 1 and raw_input('Drop tables? [yes/N] ') == 'yes':
   db.drop_all()
+
 db.create_all()
 ed_user = User('ed', 'Ed Jones', 'ed@example.com', 'password')
 db.session.add(ed_user)
